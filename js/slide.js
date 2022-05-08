73ed5cb328af34a28986c449b903c5c93f0b0054
ericthmoritsuka <ericthmoritsuka@gmail.com>
2022-05-07 23:15:17 -0300

    adicionando navegacao alternativa

--- js/slide.js
+++ js/slide.js
@@ -10,6 +10,7 @@ export default class Slide {
       movement: 0,
     };
     this.activeClass = "ativo";
+    this.changeEvent = new Event("changeEvent");
   }
 
   transition(active) {
@@ -106,6 +107,7 @@ export default class Slide {
     this.slidesIndexNav(index);
     this.position.finalPosition = activeSlide.position;
     this.changeActiveClass();
+    this.wrapper.dispatchEvent(this.changeEvent);
   }
 
   changeActiveClass() {
@@ -156,6 +158,10 @@ export default class Slide {
 }
 
 export class SlideNav extends Slide {
+  constructor(slide, wrapper) {
+    super(slide, wrapper);
+    this.bindControlEvents();
+  }
   addArrow(previous, next) {
     this.previousElement = document.querySelector(previous);
     this.nextElement = document.querySelector(next);
@@ -165,5 +171,45 @@ export class SlideNav extends Slide {
   addArrowEvent() {
     this.previousElement.addEventListener("click", this.activatePreviousSlide);
     this.nextElement.addEventListener("click", this.activateNextSlide);
+  }
+
+  createControl() {
+    const control = document.createElement("ul");
+    control.dataset.control = "slide";
+    this.slideArray.forEach((item, index) => {
+      control.innerHTML += `<li><a href="#slide${index + 1}">${
+        index + 1
+      }</a></li>`;
+    });
+    this.wrapper.appendChild(control);
+    return control;
+  }
+
+  eventControl(item, index) {
+    item.addEventListener("click", (event) => {
+      event.preventDefault();
+      this.changeSlide(index);
+    });
+    this.wrapper.addEventListener("changeEvent", this.activeControlItem);
+  }
+
+  activeControlItem() {
+    this.controlArray.forEach((item) =>
+      item.classList.remove(this.activeClass)
+    );
+    this.controlArray[this.index.active].classList.add(this.activeClass);
+  }
+
+  addControl(customControl) {
+    this.control =
+      document.querySelector(customControl) || this.createControl();
+    this.controlArray = [...this.control.children];
+    this.activeControlItem();
+    this.controlArray.forEach(this.eventControl);
+  }
+
+  bindControlEvents() {
+    this.eventControl = this.eventControl.bind(this);
+    this.activeControlItem = this.activeControlItem.bind(this);
   }
 }
