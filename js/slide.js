ee27886924404b39abad8e6f8240e40ec1fa5965
ericthmoritsuka <ericthmoritsuka@gmail.com>
2022-05-07 21:05:15 -0300

    fazendo calculos de distancia e movendo as fotos

--- js/slide.js
+++ js/slide.js
@@ -2,17 +2,37 @@ export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
+    this.position = {
+      finalPosition: 0,
+      startX: 0,
+      movement: 0,
+    };
+  }
+
+  moveSlide(positionX) {
+    this.position.movePosition = positionX;
+    this.slide.style.transform = `translate3d(${positionX}px, 0, 0)`;
+  }
+
+  updatePosition(clientX) {
+    this.position.movement = (this.position.startX - clientX) * 1.6;
+    return this.position.finalPosition - this.position.movement;
   }
 
   onStart(event) {
     event.preventDefault();
+    this.position.startX = event.clientX;
     this.wrapper.addEventListener("mousemove", this.onMove);
   }
 
-  onMove(event) {}
+  onMove(event) {
+    const finalPosition = this.updatePosition(event.clientX);
+    this.moveSlide(finalPosition);
+  }
 
   onEnd(event) {
-    this.wrapper.removeEventListener('mousemove', this.onMove)
+    this.wrapper.removeEventListener("mousemove", this.onMove);
+    this.position.finalPosition = this.position.movePosition;
   }
 
   addSlideEvents() {
@@ -29,6 +49,6 @@ export default class Slide {
   init() {
     this.bindEvents();
     this.addSlideEvents();
-    return init;
+    return this;
   }
 }
